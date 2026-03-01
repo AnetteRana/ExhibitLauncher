b9d6bb98a4e591ccf6ff8e94f842f19d5a84cd89
Anette Rana <pertrosfoliea@gmail.com>
2026-03-01 01:13:14 +0100

    Can now start a game (gave up on centering UI for now...)

--- widget.cpp
+++ widget.cpp
@@ -1,4 +1,9 @@
 #include "widget.h"
+
+// these to run another .exe
+#include <QCoreApplication>
+#include <QDir>
+
 #include "./ui_widget.h"
 #include <QShortcut>
 
@@ -20,3 +25,50 @@ Widget::~Widget()
 {
     delete ui;
 }
+
+void Widget::on_pushButton_1_pressed()
+{
+    // make sure not to open multiple! (potential issue if game somehow ends up behind menu)
+    if (myRunningGame) {
+        qDebug() << "Game already running";
+        return;
+    }
+
+
+    QString gamePath = "C:/Users/Rana/Documents/GitHub/ExhibitLauncher/Games/GjennfunnetFortid/EscapeAnno.exe";
+    myRunningGame = new QProcess();
+
+    // set to delete after closing. pointer ready to point again!
+    connect(myRunningGame, &QProcess::finished,this,[this](int,QProcess::ExitStatus){
+        qDebug() << "Game closed";
+        myRunningGame->deleteLater();
+        myRunningGame = nullptr;
+    });
+
+
+    // actually start game
+    myRunningGame->start(gamePath);
+
+    genericButton(1);
+}
+
+void Widget::on_pushButton_2_pressed()
+{
+    genericButton(2);
+}
+
+void Widget::on_pushButton_3_pressed()
+{
+    genericButton(3);
+}
+
+void Widget::on_pushButton_4_pressed()
+{
+    genericButton(4);
+}
+
+void Widget::genericButton(int myInt)
+{
+    qDebug() << "hello im button: " << myInt;
+}
+
--- widget.h
+++ widget.h
@@ -2,6 +2,7 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QProcess>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +18,16 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+private slots:
+    void on_pushButton_1_pressed();
+    void on_pushButton_2_pressed();
+    void on_pushButton_3_pressed();
+    void on_pushButton_4_pressed();
+
 private:
     Ui::Widget *ui;
+    void genericButton(int);
+
+    QProcess *myRunningGame = nullptr;
 };
 #endif // WIDGET_H
