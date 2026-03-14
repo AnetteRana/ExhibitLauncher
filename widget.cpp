dbe8c4f9d0ea7281bf7f1144935f015985cab37d
Anette Rana <pertrosfoliea@gmail.com>
2026-03-14 23:46:05 +0100

    made new qprocess child of widget. added two games

--- widget.cpp
+++ widget.cpp
@@ -26,6 +26,7 @@ Widget::~Widget()
     delete ui;
 }
 
+// the not really a VR game
 void Widget::on_pushButton_1_pressed()
 {
     // make sure not to open multiple! (potential issue if game somehow ends up behind menu)
@@ -36,7 +37,7 @@ void Widget::on_pushButton_1_pressed()
 
 
     QString gamePath = "C:/Users/Rana/Documents/GitHub/ExhibitLauncher/Games/GjennfunnetFortid/EscapeAnno.exe";
-    myRunningGame = new QProcess();
+    myRunningGame = new QProcess(this);
 
     // set to delete after closing. pointer ready to point again!
     connect(myRunningGame, &QProcess::finished,this,[this](int,QProcess::ExitStatus){
@@ -52,13 +53,56 @@ void Widget::on_pushButton_1_pressed()
     genericButton(1);
 }
 
+// Unmasked Hamar (soda collecting)
 void Widget::on_pushButton_2_pressed()
 {
+    // make sure not to open multiple! (potential issue if game somehow ends up behind menu)
+    if (myRunningGame) {
+        qDebug() << "Game already running";
+        return;
+    }
+
+    QString gamePath = "C:/Users/Rana/Documents/GitHub/ExhibitLauncher/Games/HamarUnmasked/UnmaskedHamar.exe";
+    myRunningGame = new QProcess(this);
+
+    // set to delete after closing. pointer ready to point again!
+    connect(myRunningGame, &QProcess::finished,this,[this](int,QProcess::ExitStatus){
+        qDebug() << "Game closed";
+        myRunningGame->deleteLater();
+        myRunningGame = nullptr;
+    });
+
+
+    // actually start game
+    myRunningGame->start(gamePath);
+
     genericButton(2);
 }
 
+// Loom Game
 void Widget::on_pushButton_3_pressed()
 {
+    // make sure not to open multiple! (potential issue if game somehow ends up behind menu)
+    if (myRunningGame) {
+        qDebug() << "Game already running";
+        return;
+    }
+
+    QString gamePath = "C:/Users/Rana/Documents/GitHub/ExhibitLauncher/Games/LoomGame/LoomGame.exe";
+    myRunningGame = new QProcess(this);
+
+    // set to delete after closing. pointer ready to point again!
+    connect(myRunningGame, &QProcess::finished,this,[this](int,QProcess::ExitStatus){
+        qDebug() << "Game closed";
+        myRunningGame->deleteLater();
+        myRunningGame = nullptr;
+    });
+
+
+    // actually start game
+    myRunningGame->start(gamePath);
+
+
     genericButton(3);
 }
 
